a03e399a0985e0bd3449a55afff4c186b0180548
Soka_jplr <juanlazarorp@gmail.com>
2024-06-16 21:19:27 -0600

    calificacion 1.0

--- app/src/main/java/com/example/learncook/adaptadores/CalificacionAdapter.kt
+++ app/src/main/java/com/example/learncook/adaptadores/CalificacionAdapter.kt
@@ -1,26 +1,36 @@
 package com.example.learncook.adaptadores
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.learncook.R
 import com.example.learncook.interfaces.ListenerRecycleCalificacion
 import com.example.learncook.poko.Calificacion
 
 class CalificacionAdapter (val calificaciones: List<Calificacion>, val listener : ListenerRecycleCalificacion): RecyclerView.Adapter<CalificacionAdapter.ViewHolderCalificacion>(){
 
     class ViewHolderCalificacion(itemView: View): RecyclerView.ViewHolder(itemView){
-
+        val tvNombreUsuario: TextView = itemView.findViewById(R.id.tv_nombre_usuario)
+        val tvCalificacion: TextView = itemView.findViewById(R.id.tv_calificacion)
+        val tvComentario: TextView = itemView.findViewById(R.id.tv_comentario)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderCalificacion {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_calificaciones,parent,false)
+
+        return ViewHolderCalificacion(itemView)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return calificaciones.size
     }
 
     override fun onBindViewHolder(holder: ViewHolderCalificacion, position: Int) {
-        TODO("Not yet implemented")
+        val calificacion = calificaciones.get(position)
+        holder.tvNombreUsuario.text = ""
+        holder.tvCalificacion.text = ""
+        holder.tvComentario.text =""
     }
 }
